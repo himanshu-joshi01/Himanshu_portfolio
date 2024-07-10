2a4420c9f9512f741e7748147d6a73e219ed2eee
HIMANSHU_JOSHI <127012017+himanshu-joshi01@users.noreply.github.com>
2024-07-10 15:35:34 +0530

    update details

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -78,7 +78,7 @@ function Home() {
           {tagline}
         </h2>
       </div>
-      <a href="Resume/HimanshuJoshiResume.pdf" download="" class="main-btn">Download CV</a>
+      <a href="../Resume/HimanshuJoshiResume.pdf" download="" class="main-btn">Download CV</a>
       <div className="mt-5 md:mt-0">
         <img ref={myimageref} className="w-1/2 md:ml-auto" src={img} alt="Himanshu Joshi" />
       </div>
